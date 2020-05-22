68a17efe7e6389e7fc144b3d3acd0cd412a29224
TomaAnatolievna <tomaanatolievna23@gmail.com>
2020-05-22 15:34:05 +0300

    desktop front-end

--- script.js
+++ script.js
@@ -1,42 +1,107 @@
-// Fetch all the details element.
 const details = document.querySelectorAll("details");
-
-// Add the onclick listeners.
 details.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
-    // Close all the details that are not targetDetail.
     details.forEach((detail) => {
       if (detail !== targetDetail) {
         detail.removeAttribute("open");
         if (detail.hasAttribute("id")) {
             $(".categories").remove();
-
         }
-
-        // console.log('detail = ' + detail);
-        // console.log(' HAS ID ' + detail.hasAttribute("id"));
-        // if (detail.is('[id]'))
-        // {
-        //   console.log("detail[id] = " + detail[id]);
-        //   $(".categories").remove();
-        // }
       }
     });
   });
 });
 
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-$(".heartDiv").click(function() {
-  $(this).toggleClass("heartChoose");
-  console.log("HEART");
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie(cookiename) {
+  var user=getCookie(cookiename);
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
+}
+
+
+
+
+
+
+function jsonFromJoke(joke) {
+  var mainJoke = joke.getElementsByClassName("mainJoke")[0];
+  var url = mainJoke.getElementsByClassName("jokeA")[0].getAttribute("href");
+  var jokeValue = $(mainJoke.getElementsByClassName("data")[0].getElementsByTagName("b")[0]).text();
+  var categories = joke.getElementsByClassName("categoryName");
+  var jokeJSON = `{"id": "` + joke.id + `", "url": "` + url + `", "value": "` + jokeValue + `", "categories": [` + ((categories.length > 0) ? categories[0] : "" ) + `]}`;
+  return JSON.parse(jokeJSON);
+}
+
+$(document).ready ( function () {
+  $(document).on ("click", ".heartDiv", function () {
+      $(this).addClass("heartChoose");
+      $(this).removeClass("heartDiv");
+      var joke = $(this.parentElement).clone()[0];
+      var json = jsonFromJoke(this.parentElement);
+      console.log(json);
+      joke.id = "favorite-joke-" + joke.id;
+      $(joke).addClass("jokeFav");
+      $(joke).removeClass("joke");
+
+      // check if not in cookies already
+      $(joke).appendTo(".right-part");
+      // Add to cookies
+    });
 });
 
-
-$('.heartDiv').click(function() {
-  // $(this).css('backgroundImage', 'url(path/to/img/newimage.png)');
-  console.log("HEART");
-
+$(document).ready ( function () {
+  $(document).on ("click", ".heartChoose", function () {
+      var joke = this.parentElement;
+      if (joke.getAttribute('class') == "jokeFav") {
+        var jokeId = joke.id.substring(14);
+        var mainJokeFavorite = document.getElementById(jokeId).getElementsByClassName("heartChoose")[0];
+        $(mainJokeFavorite).addClass("heartDiv");
+        $(mainJokeFavorite).removeClass("heartChoose");
+        joke.remove();
+        // TODO: REMOVE from cookies joke
+      }
+      else
+      {
+        var mainJokeFavorite = joke.getElementsByClassName("heartChoose")[0];
+        $(mainJokeFavorite).addClass("heartDiv");
+        $(mainJokeFavorite).removeClass("heartChoose");
+        
+        var favJoke = document.getElementById("favorite-joke-" + joke.id);
+        favJoke.remove();
+        // TODO: REMOVE from cookies 
+      }
+    });
 });
+
 
 
 
@@ -55,21 +120,15 @@ $('.categoryDiv').on('click','.categories',function () {
 
 
 function randomFact() {
-    // We call the Web Service via AJAX
     var xmlhttp = new XMLHttpRequest();
     var url = "https://api.chucknorris.io/jokes/random";
     xmlhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var json = JSON.parse(this.responseText);
-        // We parse the JSON response
-        // console.log("jsonRand - ", json);
-
-
+        localStorage.clear();
         createNew(json);
-        // parseJson(json);
       }
     };
-
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
   }
@@ -77,176 +136,101 @@ function randomFact() {
 
 function getCategories() {
   var xhr = new XMLHttpRequest();
-  // xhr.withCredentials = true;
   var url = "https://api.chucknorris.io/jokes/categories";
   xhr.addEventListener("readystatechange", function () {
     if(this.readyState == 4 && this.status == 200) {
-        // console.log(this.responseText);
         var json1 = JSON.parse(this.responseText);
-        // console.log(json1.length);
         for (let i = 0; i < json1.length; i++)
         {
-          // console.log (i, json1[i]);
           $( ".categoryDiv" ).append( "<span class='categories' id='"+ json1[i] +"'>" + json1[i] + "</span>" );
         }
     }
   });
-  
   xhr.open("GET", url); 
   xhr.send();
- 
 }
 
   function categoryFact() {
-    // We call the Web Service via AJAX
     var xmlhttp = new XMLHttpRequest();
     var category = $('.selected').attr('id');
     var url = "https://api.chucknorris.io/jokes/random?category=" + category;
     xmlhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var json = JSON.parse(this.responseText);
-        // We parse the JSON response
-        // console.log("jsonCat - ", json);
-
+        localStorage.clear();
         createNew(json);
-        // parseJson(json);
       }
     };
 
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-
-    // console.log(url);
   }
 
   function queryFact() {
-    // We call the Web Service via AJAX
     var xmlhttp = new XMLHttpRequest();
     var str = $("#fname").val();
     var url = "https://api.chucknorris.io/jokes/search?query=" + str;
     xmlhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var json = JSON.parse(this.responseText);
-        // We parse the JSON response
         parseSearchJson(json);
       }
     };
-
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-
-    // console.log(url);
   }
 
   
 function parseSearchJson(json) {
-  // console.log("json - ", json);
   var total = json["total"];
   if ((total).constructor === Number && Number(total) > 0) {
     var results = json["result"];
-    // parseJson(value);
+    localStorage.clear();
     results.forEach(createNew);
-    // var partHeight =  $(".left-part").height();
-    // $(".right-part").height(partHeight);
-
   }
 }
 
 
 function createNew(joke, index) {
-
-
+  var jokeUpdateTime = new Date(joke["updated_at"]);
+  var currentTime = new Date();
+  var days = Math.round((currentTime.getTime() - jokeUpdateTime.getTime()) / (1000 * 3600));
+  var update_at_text = localStorage.getItem("update_dates");
+  if (update_at_text != null) {
+    console.log(update_at_text);
+    var jokes_updates = JSON.parse(update_at_text);
+    update_at_text = `[ { "id": "` + joke["id"] +`", "updated_at": "` + joke["updated_at"] + `"} ]`;
+    jokes_updates.push({"id": joke["id"], "updated_at": joke["updated_at"]});
+    localStorage.setItem("update_dates", jokes_updates.toString());
+  } else {
+    update_at_text = `[ { "id": "` + joke["id"] +`", "updated_at": "` + joke["updated_at"] + `"} ]`;
+    localStorage.setItem("update_dates", update_at_text);
+  }
   $( ".left-part").append( `
-    <div class="joke">
+    <div class="joke" id="` + joke["id"] +`">
       <div class="heartDiv">
       </div>
       <div class="commentDiv">
       </div>
       <div class="mainJoke">
-          <a href=" ` + joke["url"] + ` " id="jokeA">ID: <span id="jokeId"> ` + joke["id"] + `</span></a>
-          <div id="data"> <b>` + joke["value"]+ ` </b></div>    
+          <a href=" ` + joke["url"] + ` " class="jokeA">ID: <span class="jokeId"> ` + joke["id"] + `</span> <img src="img/link.png" class="linkImg" alt=""> </a>
+          <div class="data"> <b>` + joke["value"]+ ` </b></div>    
       </div>
-
-      <div id="lastTime"> Last update: ` + joke["updated_at"]+ ` </div>
-      ` + ((joke["categories"].length > 0) ? ('<div id="categoryName">' + joke["categories"] + '</div>') : "") + `
+      <div class="lastTime"> Last update: ` + days + ` hours ago </div>
+      ` + ((joke["categories"].length > 0) ? ('<div class="categoryName">' + joke["categories"] + '</div>') : "") + `
     </div>
   
   ` );
-
-// resizeRightPart();
-  
 }
-
-
-// function resizeRightPart() {
-//   var leftPartHeight =  $(".left-part").height();
-//   var rightPartHeight = $(".right-part").height();
-//   var windowHeight = $(window).height();
-//   var documentHeight = $(document).height();
-//   // console.log("part = " + partHeight);
-//   // console.log("window = " + $(window).height());
-//   // if(leftPartHeight > rightPartHeight) 
-//   //   {
-//       $(".right-part").height(leftPartHeight);
-//       console.log("first");
-//     // } 
-//     // else {
-//     //   console.log("second");
-//     //   // $(".right-part").height("100%");
-//     // }   
-
-//     console.log("left = " + leftPartHeight);
-//     console.log("right = " + rightPartHeight);
-//     console.log("window = " + windowHeight);
-//     console.log("document = " + documentHeight);
-// }
-
-  // function parseJson(json) {
-  //   console.log("json = " + json);
-    // var fact = "<b>" + json["value"] + "</b>";
-    // var jokeId = json["id"];
-    // var idLink = json["url"];
-    // var jokeTime = json["updated_at"];
-    // var jokeCategory =  $('.selected').attr('id');
-    // var jokeCategory = json["categories"];
-    // document.getElementById("data").innerHTML = fact;
-    // document.getElementById("jokeId").innerHTML = jokeId;
-    // document.getElementById("categoryName").innerHTML = jokeCategory;
-    // console.log("joke category = " + jokeCategory + " "+ typeof(jokeCategory));
-    // if(typeof(jokeCategory) == "undefined") {
-    // if(jokeCategory.length > 0) {
-    //   $( "#categoryName" ).show();
-    // }
-    // else {
-    //   $( "#categoryName" ).hide();
-    // }
-
-
-
-    // $("#jokeA").attr("href", idLink);
-    // var today = new Date();
-    // var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    // var dateTime = date+' '+time;
-    // document.getElementById("lastTime").innerHTML = "Last update: " + jokeTime;
-  // }
-
-  // Finally we add a click event listener on the logo of Chuck Norris
-  // to load a new random fact when the user will click on it
-
 
 
 function deleteJokes() {
   var checkJokes = document.getElementsByClassName("joke");
-  // console.log("checkJokes here " + checkJokes );
-  // console.log("checkJokes  " + checkJokes.length );
   if(checkJokes.length > 0) {
     $(".joke").remove();
 
   }
 }
-
-
 
 
   document.getElementById("getBtn").addEventListener("click", function() {
@@ -256,7 +240,6 @@ function deleteJokes() {
         deleteJokes();
           console.log("Random joke");
         randomFact();
-        // resizeRightPart();
 
       }
      else if($("#category").prop("checked")) {
@@ -273,6 +256,8 @@ function deleteJokes() {
       }
 
   });
+
+
 
 
 
