2d3cb9981ad815780092d2193d1768e590fc437a
TomaAnatolievna <tomaanatolievna23@gmail.com>
2020-05-24 03:10:31 +0300

    bugs fixed

--- script.js
+++ script.js
@@ -2,36 +2,44 @@ window.onresize = windowResize;
 window.onload = windowResize;
 
 function windowResize () {
-  if ($(document).width() <= 850) {
+  console.log("window" + $(window).width());
+  console.log("document" + $(document).width());
+  console.log("document height " + $(document).height());
 
+  console.log("window screen" + window.screen.height);
+
+  if ($(document).width() < 850) {
     $("#overlay").addClass("right-part-tablet overlay");
     $("#overlay").removeClass("right-part");
-
   }
   else {
     $("#overlay").addClass("right-part");
     $("#overlay").removeClass("right-part-tablet overlay");
   }
+  resizeRightPart();
 }
+
 
 $('#toggle').click(function() {
   $(this).toggleClass('active');
   $('#overlay').toggleClass('open');
   $('#navFull').toggleClass('navFull');
- });
-
-
+  if (document.getElementById("overlay").getAttribute("class").includes("open")) {
+    document.getElementsByTagName('html')[0].style.overflow = "hidden";
+  }
+  else  
+  document.getElementsByTagName('html')[0].style.overflow = "";
+});
 
 var lastScrollTop = 0;
 
 $(document.getElementById("overlay")).scroll(function() {
 
     var scrollTop = $(this).scrollTop();
-    console.log("test", scrollTop);
     if (scrollTop > lastScrollTop && scrollTop < 75) {
-      document.getElementById("button_container_main").style.top = 40-scrollTop + "px";
-    } else if (scrollTop > lastScrollTop || (scrollTop < lastScrollTop && scrollTop > 75)) {
-      document.getElementById("button_container_main").style.top = "-4%";
+      document.getElementById("button_container_main").style.top = 40 - scrollTop + "px";
+    } else if (scrollTop > lastScrollTop || (scrollTop < lastScrollTop && scrollTop >= 75)) {
+      document.getElementById("button_container_main").style.top = "-40px";
     } else {
       document.getElementById("button_container_main").style.top = 40-scrollTop + "px";
     }
@@ -54,54 +62,90 @@ details.forEach((targetDetail) => {
   });
 });
 
-function setCookie(cname,cvalue,exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-function checkCookie(cookiename) {
-  var user=getCookie(cookiename);
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-     user = prompt("Please enter your name:","");
-     if (user != "" && user != null) {
-       setCookie("username", user, 30);
-     }
-  }
-}
-
-
-
-
-
-
 function jsonFromJoke(joke) {
   var mainJoke = joke.getElementsByClassName("mainJoke")[0];
   var url = mainJoke.getElementsByClassName("jokeA")[0].getAttribute("href");
   var jokeValue = $(mainJoke.getElementsByClassName("data")[0].getElementsByTagName("b")[0]).text();
   var categories = joke.getElementsByClassName("categoryName");
-  var jokeJSON = `{"id": "` + joke.id + `", "url": "` + url + `", "value": "` + jokeValue + `", "categories": [` + ((categories.length > 0) ? categories[0] : "" ) + `]}`;
   
+  var update_at_text = localStorage.getItem("update_dates");
+  var jokes_updates = JSON.parse(update_at_text);
+  var updated_at = "";
+  for (const joke_update of jokes_updates) {
+    if (joke_update["id"] == joke.id) {
+      updated_at = joke_update["updated_at"];
+    }
+  }
+
+  var jokeJSON = `{"id": "` + joke.id + `",
+  "url": "`+ url +`", "value": "` + jokeValue + `",
+  "categories": [` + ((categories.length > 0) ? "\"" + categories[0].innerText + "\"" : "" ) + `],
+  "updated_at": "` + updated_at + `"}`;
   return JSON.parse(jokeJSON);
+}
+
+function removeJokeFromFavorite(jokeId) {
+  var favorite_jokes = getFavoriteJokes();
+  favorite_jokes = favorite_jokes.filter(function(joke, idx) {
+    return joke["id"] != jokeId;
+  });
+  localStorage.setItem("favorite_jokes", JSON.stringify(favorite_jokes));
+}
+
+function addJokeToFavorite(joke) {
+  var favorite_jokes = getFavoriteJokes();
+  favorite_jokes.unshift(joke);
+  localStorage.setItem("favorite_jokes", JSON.stringify(favorite_jokes));
+}
+
+function getFavoriteJokes() {
+  var favoriteJokes = localStorage.getItem("favorite_jokes");
+  if (favoriteJokes != null) {
+    var favoriteJokesJSON = JSON.parse(favoriteJokes);
+    return favoriteJokesJSON;
+  } else {
+    return [];
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  var favorite_jokes = getFavoriteJokes();
+  console.log("fav", favorite_jokes);
+  for (const joke of favorite_jokes) {
+  
+    var jokeUpdateTime = new Date(joke["updated_at"]);
+    var currentTime = new Date();
+    var days = Math.round((currentTime.getTime() - jokeUpdateTime.getTime()) / (1000 * 3600));
+    $(".rightJokes").append(
+      `<div class="jokeFav" id="favorite-joke-` + joke["id"] +`">
+      <div class="heartChoose">
+        </div>
+        <div class="comJoke">  
+
+        <div class="commentDiv">
+        </div>
+        <div class="mainJoke">
+            <a href=" ` + joke["url"] + ` " class="jokeA">ID: <span class="jokeId"> ` + joke["id"] + `</span> <img src="img/link.png" class="linkImg" alt=""> </a>
+            <div class="data"> <b>` + joke["value"]+ ` </b></div>    
+        </div>
+        </div>
+        <div class="lastTimeCat">
+        <div class="lastTime"> Last update: ` + days + ` hours ago </div>
+        ` + ((joke["categories"].length > 0) ? ('<div class="categoryName">' + joke["categories"] + '</div>') : "") + `
+      </div> </div>`);
+  }
+});
+
+function isJokeWasAdded(jokeId) {
+  var favorite_jokes = getFavoriteJokes();
+  if (favorite_jokes.length > 0) {
+    for (const favJoke of favorite_jokes) {
+      if (favJoke["id"] == jokeId) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 $(document).ready ( function () {
@@ -110,14 +154,16 @@ $(document).ready ( function () {
       $(this).removeClass("heartDiv");
       var joke = $(this.parentElement).clone()[0];
       var json = jsonFromJoke(this.parentElement);
-      console.log(json);
       joke.id = "favorite-joke-" + joke.id;
       $(joke).addClass("jokeFav");
       $(joke).removeClass("joke");
-
-      // check if not in cookies already
-      $(joke).prependTo(".rightJokes");
-      // Add to cookies
+      console.log(json);
+      if (!isJokeWasAdded(json["id"])) {
+        
+        $(joke).prependTo(".rightJokes");
+        addJokeToFavorite(json);
+      }
+      resizeRightPart();
     });
 });
 
@@ -126,17 +172,15 @@ $(document).ready ( function () {
       var joke = this.parentElement;
       if (joke.getAttribute('class') == "jokeFav") {
         var jokeId = joke.id.substring(14);
-        var mainJokeFavorite = document.getElementById(jokeId).getElementsByClassName("heartChoose")[0];
-        $(mainJokeFavorite).addClass("heartDiv");
-        $(mainJokeFavorite).removeClass("heartChoose");
+        var mainJokeFavorite = document.getElementById(jokeId);
+        if (mainJokeFavorite != null) {
+          mainJokeFavorite = mainJokeFavorite.getElementsByClassName("heartChoose")[0];
+          $(mainJokeFavorite).addClass("heartDiv");
+          $(mainJokeFavorite).removeClass("heartChoose");
+        }
         joke.remove();
-        // if (joke.parentElement.parentElement.getAttribute("class").includes("right-part-tablet")) {
-
-        // } else if (joke.parentElement.parentElement.getAttribute("class").includes("right-part")) {
-          
-        // }
-        
-        // TODO: REMOVE from cookies joke
+        removeJokeFromFavorite(jokeId);
+        resizeRightPart();
       }
       else
       {
@@ -146,18 +190,17 @@ $(document).ready ( function () {
         
         var favJoke = document.getElementById("favorite-joke-" + joke.id);
         favJoke.remove();
-        // TODO: REMOVE from cookies 
+        removeJokeFromFavorite(joke.id);
       }
+      resizeRightPart();
     });
 });
-
-
 
 
 $(document).ready(function(){
   $('input[type=radio]').click(function(){
       if (this.id == "category")
-              getCategories();
+        getCategories();
   });
 });
 
@@ -169,18 +212,18 @@ $('.categoryDiv').on('click','.categories',function () {
 
 
 function randomFact() {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://api.chucknorris.io/jokes/random";
-    xmlhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200) {
-        var json = JSON.parse(this.responseText);
-        localStorage.clear();
-        createNew(json);
-      }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-  }
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://api.chucknorris.io/jokes/random";
+  xmlhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      var json = JSON.parse(this.responseText);
+      localStorage.removeItem("update_dates");
+      createNew(json);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
 
 
 function getCategories() {
@@ -199,77 +242,78 @@ function getCategories() {
   xhr.send();
 }
 
-  function categoryFact() {
-    var xmlhttp = new XMLHttpRequest();
-    var category = $('.selected').attr('id');
-    var url = "https://api.chucknorris.io/jokes/random?category=" + category;
-    xmlhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200) {
-        var json = JSON.parse(this.responseText);
-        localStorage.clear();
-        createNew(json);
-      }
-    };
+function categoryFact() {
+  var xmlhttp = new XMLHttpRequest();
+  var category = $('.selected').attr('id');
+  var url = "https://api.chucknorris.io/jokes/random?category=" + category;
+  xmlhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      var json = JSON.parse(this.responseText);
+      localStorage.removeItem("update_dates");
+      createNew(json);
+    }
+  };
 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
 
-  function queryFact() {
-    var xmlhttp = new XMLHttpRequest();
-    var str = $("#fname").val();
-    var url = "https://api.chucknorris.io/jokes/search?query=" + str;
-    xmlhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200) {
-        var json = JSON.parse(this.responseText);
-        parseSearchJson(json);
-      }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-  }
+function queryFact() {
+  var xmlhttp = new XMLHttpRequest();
+  var str = $("#fname").val();
+  var url = "https://api.chucknorris.io/jokes/search?query=" + str;
+  xmlhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      var json = JSON.parse(this.responseText);
+      parseSearchJson(json);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
 
   
 function parseSearchJson(json) {
   var total = json["total"];
   if ((total).constructor === Number && Number(total) > 0) {
     var results = json["result"];
-    localStorage.clear();
+    localStorage.removeItem("update_dates");
     results.forEach(createNew);
   }
 }
+`1`
 
-
-function createNew(joke, index) {
+function createNew(joke, index, ) {
   var jokeUpdateTime = new Date(joke["updated_at"]);
   var currentTime = new Date();
   var days = Math.round((currentTime.getTime() - jokeUpdateTime.getTime()) / (1000 * 3600));
   var update_at_text = localStorage.getItem("update_dates");
   if (update_at_text != null) {
-    console.log(update_at_text);
     var jokes_updates = JSON.parse(update_at_text);
-    update_at_text = `[ { "id": "` + joke["id"] +`", "updated_at": "` + joke["updated_at"] + `"} ]`;
     jokes_updates.push({"id": joke["id"], "updated_at": joke["updated_at"]});
-    localStorage.setItem("update_dates", jokes_updates.toString());
+    localStorage.setItem("update_dates", JSON.stringify(jokes_updates));
   } else {
     update_at_text = `[ { "id": "` + joke["id"] +`", "updated_at": "` + joke["updated_at"] + `"} ]`;
     localStorage.setItem("update_dates", update_at_text);
   }
-  $( ".left-part").append( `
-    <div class="joke" id="` + joke["id"] +`">
-      <div class="heartDiv">
+  $(".left-part").append(
+    `<div class="joke" id="` + joke["id"] +`">
+      <div class="`+ (isJokeWasAdded(joke["id"]) ? "heartChoose" : "heartDiv") + `">
       </div>
+      <div class="comJoke">  
+
       <div class="commentDiv">
       </div>
       <div class="mainJoke">
           <a href=" ` + joke["url"] + ` " class="jokeA">ID: <span class="jokeId"> ` + joke["id"] + `</span> <img src="img/link.png" class="linkImg" alt=""> </a>
           <div class="data"> <b>` + joke["value"]+ ` </b></div>    
       </div>
+      </div>
+      <div class="lastTimeCat">
       <div class="lastTime"> Last update: ` + days + ` hours ago </div>
       ` + ((joke["categories"].length > 0) ? ('<div class="categoryName">' + joke["categories"] + '</div>') : "") + `
-    </div>
-  
-  ` );
+    </div></div>`);
+    resizeRightPart();
 }
 
 
@@ -282,6 +326,30 @@ function deleteJokes() {
 }
 
 
+function resizeRightPart() {
+  
+  var body = document.body;
+  var html = document.documentElement;
+
+    console.log( body.scrollHeight);
+    console.log( body.offsetHeight);
+    console.log( html.clientHeight);
+    console.log( html.scrollHeight);
+    console.log( html.offsetHeight);
+
+  if((body.scrollHeight > body.offsetHeight) && ($(document).width() >= 850)) {
+    document.getElementById("overlay").style.height = document.getElementById("left-part").offsetHeight + "px";
+    if(document.getElementById("overlay").offsetHeight < body.offsetHeight) {
+      document.getElementById("overlay").style.height = "100%";
+    }
+  }
+  else {
+    console.log("HERE 2");
+    document.getElementById("overlay").style.height = "100%";
+  }
+}
+
+
   document.getElementById("getBtn").addEventListener("click", function() {
   
     if($("#random").prop("checked")) {
@@ -289,26 +357,19 @@ function deleteJokes() {
         deleteJokes();
           console.log("Random joke");
         randomFact();
-
       }
      else if($("#category").prop("checked")) {
-      // resizeRightPart();
        deleteJokes();
         console.log("Category joke!");
         categoryFact();
       }
       else {
-        // resizeRightPart();
         deleteJokes();
         console.log("Query joke!");
         queryFact();
       }
 
   });
-
-
-
-
 
   randomFact();
 
